a1eb1f07217179c121712b5aa0cde9889b5e68fb
Israel Diaz <diazidx@users.noreply.github.com>
2020-08-08 00:53:09 -0400

    Proyecto_REV_0
    Revision 0 del proyecto.

--- app.py
+++ app.py
@@ -5,39 +5,97 @@ import os
 import glob
 import re
 from pathlib import Path
-
-
+import numpy as np
+import cv2
+import os
 
 # Import fast.ai Library
-from fastai import *
-from fastai.vision import *
+# from fastai import *
+# from fastai.vision import *
 
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
 
-
 # Define a flask app
 app = Flask(__name__)
 
 
-path = Path("models")
-learn = load_learner(path)
-
-
+# path = Path("models")
+# learn = load_learner(path)
 
 
 def model_predict(img_path):
     """
        model_predict will return the preprocessed image
     """
-   
-    img = open_image(img_path)
-    pred_class,pred_idx,outputs = learn.predict(img)
-    return str(pred_class)
-    
+    AGE_BUCKETS = ["(0-2)", "(4-6)", "(8-12)", "(15-20)", "(25-32)", "(38-43)", "(48-53)", "(60-100)"]
 
+    # load our serialized face detector model from disk
+    # print("[INFO] loading face detector model...")
+    prototxtPath = "models/face_detector/deploy.prototxt"
+    weightsPath = "models/face_detector/res10_300x300_ssd_iter_140000.caffemodel"
+    faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
+    # load our serialized age detector model from disk
+    # print("[INFO] loading age detector model...")
+    prototxtPath = "models/age_detector/age_deploy.prototxt"
+    weightsPath = "models/age_detector/age_net.caffemodel"
+    ageNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+    # load the input image and construct an input blob for the image
+    image = cv2.imread(img_path)
+    (h, w) = image.shape[:2]
+    blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300), (104.0, 177.0, 123.0))
+
+    # pass the blob through the network and obtain the face detections
+    # print("[INFO] computing face detections...")
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
+
+    # loop over the detections
+    for i in range(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with the
+        # prediction
+        confidence = detections[0, 0, i, 2]
+
+        # filter out weak detections by ensuring the confidence is
+        # greater than the minimum confidence
+        if confidence > 0.5:
+            # compute the (x, y)-coordinates of the bounding box for the
+            # object
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # extract the ROI of the face and then construct a blob from
+            # *only* the face ROI
+            face = image[startY:endY, startX:endX]
+            faceBlob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), (78.4263377603, 87.7689143744, 114.895847746),
+                                             swapRB=False)
+
+            # make predictions on the age and find the age bucket with
+            # the largest corresponding probability
+            ageNet.setInput(faceBlob)
+            preds = ageNet.forward()
+            i = preds[0].argmax()
+            age = AGE_BUCKETS[i]
+            ageConfidence = preds[0][i]
+
+            # display the predicted age to our terminal
+            text = "{}: {:.2f}%".format(age, ageConfidence * 100)
+            # print("[INFO] {}".format(text))
+
+            # draw the bounding box of the face along with the associated
+            # predicted age
+            y = startY - 10 if startY - 10 > 10 else startY + 10
+            cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+            cv2.putText(image, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+        # display the output image
+        cv2.imwrite("uploads/result.png", image)
+    return text
+    # cv2.imshow("Image", image)
+    # cv2.waitKey(0)
 
 
 @app.route('/', methods=['GET'])
@@ -63,9 +121,5 @@ def upload():
         return preds
     return None
 
-
 if __name__ == '__main__':
-    
     app.run()
-
-
