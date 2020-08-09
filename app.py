0fc34fde311b59056f4aabbb7e3d875ff65e4726
Israel Diaz <diazidx@users.noreply.github.com>
2020-08-09 11:19:24 -0400

    update v1

--- app.py
+++ app.py
@@ -107,5 +107,5 @@ def upload():
         return preds
     return None
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0/0", port = 5000)
